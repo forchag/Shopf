7a68605fec54207db80f3607c986c955292d34bf
forchag <glenforcha@gmail.com>
2025-10-07 18:00:12 +0100

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config("SECRET_KEY", default="test")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["164.68.123.184"]
 
 
 # Application definition
